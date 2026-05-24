0ca54a149c311859965a42e6b6790dc085d952ca
sugi1410 <suganthi1410@gmail.com>
2026-05-24 10:55:05 -0500

    Objects accessing and modifying properties exercises done

--- tutorials/08-objects/accessing-modifying-properties/exercises/practice.js
+++ tutorials/08-objects/accessing-modifying-properties/exercises/practice.js
@@ -13,15 +13,23 @@ const communicationSpecs = {
 };
 
 // TODO: Update the 'frequencyGhz' to 9.1 using dot notation.
+communicationSpecs.frequencyGhz = 9.1;
+console.log("New value for frequencyGhz is", communicationSpecs.frequencyGhz);
 
 // TODO: Update the 'encryption-protocol' to "RSA-4096" using bracket notation.
-
+communicationSpecs["encryption-protocol"] = "RSA-4096";
+console.log(`Encryption prtocol updated as ${communicationSpecs["encryption-protocol"]}`);
 // TODO: Log a full statement confirming the update using both bracket notation and dot notation.
+console.log(`confirmed Frequency and encryption-protocol are frquencyGhz : ${communicationSpecs.frequencyGhz} and encryption-protocol : ${communicationSpecs["encryption-protocol"]}`);
 
 
 /** EXERCISE 2: POPULATING ENEMY DATA **/
 
-const scanningDrone = {};
+const scanningDrone = {
+    classification : "Frigate",
+    weaponCount : 12,
+    "shield-active" : false,
+};
 
 /*
     TODO: The drone scanned an unidentified object. Add three keys to the 
@@ -30,8 +38,13 @@ const scanningDrone = {};
     - weaponCount (Set to 12)
     - "shield-active" (Set to false)
     
+
     Then, log a full statement using some of the properties of the object.
 */
+console.log("classification : ",scanningDrone.classification);
+console.log("WeaponCount : ", scanningDrone.weaponCount);
+console.log(`shield-active : ${scanningDrone["shield-active"]}`);
+
 
 /** EXERCISE 3: THE TERMINAL SELECTOR **/
 
@@ -47,3 +60,4 @@ let userTerminalQuery = "oxygenLevel";
     TODO: Use the 'userTerminalQuery' variable to dynamically log the correct 
     stat from the 'airlockControls' object using bracket notation.
 */
+console.log("Oxygen level : " , airlockControls[userTerminalQuery]);
