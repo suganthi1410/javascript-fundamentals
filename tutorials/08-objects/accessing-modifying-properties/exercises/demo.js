b44271b26a2795a82349d240a2941b57ac7cfd39
sugi1410 <suganthi1410@gmail.com>
2026-05-23 22:41:07 -0500

    object accessing modifying properties demo done

--- tutorials/08-objects/accessing-modifying-properties/exercises/demo.js
+++ tutorials/08-objects/accessing-modifying-properties/exercises/demo.js
@@ -9,18 +9,32 @@ const cargoBay = {
 };
 
 // DEMO: Use dot notation to modify the tonnage
+cargoBay.tonnage=200;
+console.log("New value given for tonnage:",cargoBay.tonnage);
 
 // DEMO: Use dot notation to add a technician property
+cargoBay.technician= "JOhn";
+console.log(cargoBay); 
+// output : {
+ // manifestId: 'CB-04',
+  //'containment-status': 'SECURE',
+  //tonnage: 200,
+  //technician: 'JOhn'
+//}
 
 // DEMO: Use dot notation to form a sentence using properties of the object
 // and log it to the console.
+console.log(`cargoBay ${cargoBay.manifestId} is handled by the technician ${cargoBay.technician}`);
+//Output: cargoBay CB-04 is hadled by the technician JOhn
 
 // DEMO: Use bracket notation to update the containment status
+cargoBay["containment-status"] = "unsecure";
 
 // DEMO: Use bracket notation to print an alert about the containment status
+console.log(`⚠️  Alert: containment-status is ${cargoBay["containment-status"]}`);
+//Output: ⚠️  Alert: containment-status is unsecure
 
 /** EXAMPLE 2 - DYNAMIC VARIABLE LOOKUPS **/
-
 const engineCore = {
     temperature: 3200,
     pressure: 150,
@@ -31,7 +45,12 @@ let statRequested = "pressure";
 
 // DEMO: What happens if we try to use dot notation here?
 
+console.log(engineCore.stateRequested);
+//Output : undefined
+
 // DEMO: Use the dynamic key to get the stat
+console.log(engineCore[statRequested]);
+//Output : 150
 
 // DEMO: Make a git commit!
 
